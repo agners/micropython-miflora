92d4a7b336b3dab91effebfe3c3762ed50383d36
Stefan Agner <stefan@agner.ch>
2020-04-12 23:24:09 +0200

    make ble_mi_flora.py standalone/enhance retry logic

---
+++ README.md
@@ -0,0 +1,8 @@
+# Mi Flora MicroPython library
+
+MicroPython library for Xiaomi Mi Flora BLE plant sensors.
+
+Currently this crashes quite consistently when running on a ESP32. Causes are
+under investigations, see also https://github.com/micropython/micropython/issues/5686.
+
+Patches/fixes welcome!
--- ble_mi_flora.py
+++ ble_mi_flora.py
@@ -1,12 +1,11 @@
-# This example finds and connects to a BLE temperature sensor (e.g. the one in ble_temperature.py).
+# BLE Class which allows to connect to Mi Flora BLE plant sensors
+# Based on the ble_temperature_central.py example.
 
 import bluetooth
 import random
 import struct
 import time
 import micropython
-
-from ble_advertising import decode_services, decode_name
 
 from micropython import const
 
@@ -43,10 +42,48 @@ _FLORA_CMD_CHAR = bluetooth.UUID("00001a00-0000-1000-8000-00805f9b34fb")
 _FLORA_DATA_CHAR = bluetooth.UUID("00001a01-0000-1000-8000-00805f9b34fb")
 _FLORA_FIRM_CHAR = bluetooth.UUID("00001a02-0000-1000-8000-00805f9b34fb")
 
+# Advertising payloads are repeated packets of the following form:
+#   1 byte data length (N + 1)
+#   1 byte type (see constants below)
+#   N bytes type-specific data
+
+_ADV_TYPE_FLAGS = const(0x01)
+_ADV_TYPE_NAME = const(0x09)
+_ADV_TYPE_UUID16_COMPLETE = const(0x3)
+_ADV_TYPE_UUID32_COMPLETE = const(0x5)
+_ADV_TYPE_UUID128_COMPLETE = const(0x7)
+_ADV_TYPE_UUID16_MORE = const(0x2)
+_ADV_TYPE_UUID32_MORE = const(0x4)
+_ADV_TYPE_UUID128_MORE = const(0x6)
+
+def decode_field(payload, adv_type):
+    i = 0
+    result = []
+    while i + 1 < len(payload):
+        if payload[i + 1] in adv_type:
+            result.append(payload[i + 2 : i + payload[i] + 1])
+        i += 1 + payload[i]
+    return result
+
+
+def decode_name(payload):
+    n = decode_field(payload, [_ADV_TYPE_NAME])
+    return str(n[0], "utf-8") if n else ""
+
+
+def decode_services(payload):
+    services = []
+    for u in decode_field(payload, [_ADV_TYPE_UUID16_COMPLETE, _ADV_TYPE_UUID16_MORE]):
+        services.append(bluetooth.UUID(struct.unpack("<h", u)[0]))
+    for u in decode_field(payload, [_ADV_TYPE_UUID32_COMPLETE, _ADV_TYPE_UUID32_MORE]):
+        services.append(bluetooth.UUID(struct.unpack("<d", u)[0]))
+    for u in decode_field(payload, [_ADV_TYPE_UUID128_COMPLETE, _ADV_TYPE_UUID128_MORE]):
+        services.append(bluetooth.UUID(u))
+    return services
+
 class BLEMiFloraCentral:
     def __init__(self, ble):
         self._ble = ble
-        self._ble.active(True)
         self._ble.irq(handler=self._irq)
 
         # Cached name and address from a successful scan.
@@ -94,10 +131,6 @@ class BLEMiFloraCentral:
                 # Found a Mi Flora device, remember it
                 self._devices[device_tuple] = decode_name(adv_data) or "?"
 
-                self._addr_type = addr_type
-                self._addr = bytes(addr)
-                self._name = decode_name(adv_data) or "?"
-
         elif event == _IRQ_SCAN_COMPLETE:
             self._scanning = False
             if self._scan_callback:
@@ -121,27 +154,26 @@ class BLEMiFloraCentral:
         elif event == _IRQ_PERIPHERAL_DISCONNECT:
             # Disconnect (either initiated by us or the remote end).
             conn_handle, _, _, = data
+
             print("_IRQ_PERIPHERAL_DISCONNECT")
+            # Somehow we get sometimes a disconnect event altough we never got
+            # a connect... It seems that connecting aborted somehow.
+            self._connecting = False
+
             if conn_handle == self._conn_handle:
-                print("not our handle?!")
                 # If it was initiated by us, it'll already be reset.
                 self._reset()
 
         elif event == _IRQ_GATTC_SERVICE_RESULT:
             # Connected device returned a service.
             conn_handle, start_handle, end_handle, uuid = data
-            print("_IRQ_GATTC_SERVICE_RESULT", uuid);
             if conn_handle == self._conn_handle and uuid == _ENV_FLORA_DATA_UUID:
-                print("_IRQ_GATTC_SERVICE_RESULT, self._ble.gattc_discover_characteristics", uuid)
-                print(start_handle, end_handle)
                 self._start_handle = start_handle
                 self._end_handle = end_handle
-                print("Service discovered")
 
         elif event == _IRQ_GATTC_CHARACTERISTIC_RESULT:
             # Connected device returned a characteristic.
             conn_handle, def_handle, value_handle, properties, uuid = data
-            print("Characteristic result", uuid)
             if conn_handle == self._conn_handle:
                 if uuid == _FLORA_CMD_CHAR:
                     self._cmd_handle = value_handle
@@ -149,8 +181,9 @@ class BLEMiFloraCentral:
                     self._data_handle = value_handle
                 if uuid == _FLORA_FIRM_CHAR:
                     self._firm_handle = value_handle
+
                 # We've finished connecting and discovering device, fire the connect callback.
-                if self._conn_callback:
+                if self._conn_callback and self.is_characteristic_discovered():
                     self._conn_callback()
 
         elif event == _IRQ_GATTC_READ_RESULT:
@@ -186,11 +219,11 @@ class BLEMiFloraCentral:
         return self._connecting
 
     def is_service_discovered(self):
-        print("is_service_discovered", self._start_handle, self._end_handle)
         return self._start_handle is not None and self._end_handle is not None
 
     def is_characteristic_discovered(self):
-	return self._firm_handle is not None
+	return (self._firm_handle is not None and self._cmd_handle is not None
+                and self._data_handle is not None)
 
     def start_characteristic_discover(self):
         print("start_characteristic_discover")
@@ -202,7 +235,7 @@ class BLEMiFloraCentral:
     def scan(self, duration_ms=20000, callback=None):
         self._scan_callback = callback
         self._scanning = True
-        self._ble.gap_scan(duration_ms, 30000, 30000)
+        self._ble.gap_scan(duration_ms, 30000, 11250)
 
     def is_scan_finished(self):
         return not self._scanning
@@ -212,15 +245,14 @@ class BLEMiFloraCentral:
 
     # Connect to the specified device (otherwise use cached address from a scan).
     def connect(self, addr_type=None, addr=None, callback=None):
-        self._addr_type = addr_type or self._addr_type
-        self._addr = addr or self._addr
+        self._addr_type = addr_type
+        self._addr = addr
         self._conn_callback = callback
         if self._addr_type is None or self._addr is None:
             return False
-        print("Connecting...", self._addr_type, self._addr)
-        self._ble.gap_connect(self._addr_type, self._addr)
+        print(self._conn_handle)
+        print(self._ble.gap_connect(self._addr_type, self._addr, 5000))
         self._connecting = True
-        print("Done")
         return True
 
     # Disconnect from current device.
@@ -228,7 +260,6 @@ class BLEMiFloraCentral:
         if not self._conn_handle:
             return
         self._ble.gap_disconnect(self._conn_handle)
-        self._reset()
 
     # Issues an (asynchronous) write which then will trigger a read
     def read_sensor_values(self, callback):
@@ -239,7 +270,7 @@ class BLEMiFloraCentral:
         value = struct.pack("<h", 0x1fa0)
         self._ble.gattc_write(self._conn_handle, self._cmd_handle, value, 1)
 
-    def read_battery(self, callback):
+    def read_firmware(self, callback):
         if not self.is_connected():
             return
 
@@ -270,6 +301,7 @@ class BLEMiFloraCentral:
 
 def demo():
     ble = bluetooth.BLE()
+    ble.active(True)
     central = BLEMiFloraCentral(ble)
 
     print("Scanning for Mi Flora devices...")
@@ -284,48 +316,102 @@ def demo():
         addr_type, addr = device_key
         print("MAC: ", "".join('{:02x}:'.format(x) for x in addr)[:-1])
 
-        central.connect(addr_type, addr)
+    # Now get sensor values for each device
+    for device_key, device_name in central.get_devices().items():
+        addr_type, addr = device_key
 
-        # Wait for connection...
-        while not central.is_connected() and central.is_connecting():
-            time.sleep_ms(100)
+        # Connetion is not always successful, so try multiple times...
+        for attempt in range(5):
+            if not attempt == 0:
+                print("Connection unsuccessful (attempt {0})".format(attempt + 1))
+                central.disconnect()
+                while central.is_connected():
+                    time.sleep_ms(100)
 
-        if not central.is_connected():
-            print("Connection unsuccessful")
-            continue
+                time.sleep_ms(1000)
 
-        print("Connected")
+            print("Connecting to ", "".join('{:02x}:'.format(x) for x in addr)[:-1])
+            central.connect(addr_type, addr)
 
-        # Wait for service discovery...
-        while not central.is_service_discovered():
-            time.sleep_ms(100)
-            print(".")
+            # Wait for connection...
+            while not central.is_connected() and central.is_connecting():
+                time.sleep_ms(100)
 
-        print("Service discovered main loop")
+            if not central.is_connected():
+                continue
 
-        # Work around bug in ESP IDF NimBLE implementation:
-        # Wait for service discovery to complete before triggering charactristic
-        # discovery.
-        # https://github.com/espressif/esp-idf/issues/4913
-        time.sleep_ms(500)
+            print("Connected")
 
-        central.start_characteristic_discover()
+            # Wait for service discovery...
+            while not central.is_service_discovered() and central.is_connected():
+                time.sleep_ms(100)
+                print(".")
 
-        while not central.is_characteristic_discovered():
-            time.sleep_ms(100)
-            print(".")
+            if not central.is_connected():
+                continue
+            print("Service discovered main loop")
 
-        print("Charactristics we are looking for discovered")
+            # Work around bug in ESP IDF NimBLE implementation:
+            # Wait for service discovery to complete before triggering charactristic
+            # discovery.
+            # https://github.com/espressif/esp-idf/issues/4913
+            time.sleep_ms(1500)
 
-        # Work around same issue here...
-        time.sleep_ms(500)
+            central.start_characteristic_discover()
 
-        # Explicitly issue reads, using "print" as the callback.
-        central.read_sensor_values(callback=print)
-        central.read_battery(callback=print)
+            while not central.is_characteristic_discovered() and central.is_connected():
+                time.sleep_ms(100)
+                print(".")
 
-        central.disconect()
-        print("Disconnected")
+            if not central.is_connected():
+                continue
+
+            print("Charactristics we are looking for discovered")
+
+            # Work around same issue here...
+            time.sleep_ms(3000)
+
+            # Explicitly issue reads, using "print" as the callback.
+            print("Reading values")
+
+            sensor_values_received = False
+            def sensor_values(temperature, light, moisture, conductivity):
+                print("Temperature: {0}°C".format(temperature))
+                print("Light: {0} lux".format(light))
+                print("Moisture: {0}%".format(moisture))
+                print("Conductivity: {0}uS/cm".format(conductivity))
+                nonlocal sensor_values_received
+                sensor_values_received = True
+
+            central.read_sensor_values(callback=sensor_values)
+
+            firmware_values_received = False
+            def firmware_values(battery, firmware_version):
+                print("Battery: {0}%".format(battery))
+                print("Firmware Version: {0}".format(firmware_version))
+                nonlocal firmware_values_received
+                firmware_values_received = True
+
+            central.read_firmware(callback=firmware_values)
+
+            while ((not sensor_values_received or not firmware_values_received)
+                   and central.is_connected()):
+                time.sleep_ms(100)
+                print(".")
+
+            if not central.is_connected():
+                continue
+            central.disconnect()
+
+            # This is important otherwise the BLE stack won't allow us to
+            # connect to another device.
+            while central.is_connected():
+                time.sleep_ms(100)
+
+            print("Disconnected")
+
+            # Successful attempt, break retry looop
+            break
 
 
 if __name__ == "__main__":
